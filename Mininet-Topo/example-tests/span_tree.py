7c2e29bcff47d6b46b9ccdfe31bcb020e8e68eb0
山清水秀 <wmy_one@163.com>
2018-04-18 12:08:09 +0800

    Add files via upload

---
+++ Mininet-Topo/example-tests/span_tree.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from mininet.cli import CLI
+from mininet.net import Mininet
+from mininet.node import RemoteController
+from mininet.link import Link
+from mininet.term import makeTerm
+
+if __name__ == '__main__':
+
+    net = Mininet(controller=RemoteController)
+
+    c0 = net.addController('c0')
+
+    s1 = net.addSwitch('s1')
+    s2 = net.addSwitch('s2')
+    s3 = net.addSwitch('s3')
+
+    h1 = net.addHost('h1')
+    h2 = net.addHost('h2')
+    h3 = net.addHost('h3')
+
+    Link(s1,h1)
+    Link(s2,h2)
+    Link(s3,h3)
+
+    Link(s1,s2)
+    Link(s2,s3)
+    Link(s3,s1)
+
+    net.build()
+    c0.start()
+    s1.start([c0])
+    s2.start([c0])
+    s3.start([c0])
+
+#net.startTerms()
+
+    CLI(net)
+
+    net.stop()
